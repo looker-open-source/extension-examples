78e2d0eb44b0c394104f29a1e16d7ec5f45d9d7f
Bryn Ryans <bryn.ryans@looker.com>
2021-03-30 11:13:22 -0700

    Upgrade helloworld-ts packages and build.

--- react/typescript/helloworld-ts/webpack.develop.js
+++ react/typescript/helloworld-ts/webpack.develop.js
@@ -22,47 +22,33 @@
  * THE SOFTWARE.
  */
 
-module.exports = (api) => {
-  api.cache(true)
+const commonConfig = require('./webpack.config')
 
-  return {
-    presets: [
-      [
-        '@babel/env',
-        {
-          targets: {
-            esmodules: true,
-          },
-          modules: false,
-        },
-      ],
-      [
-        '@babel/preset-react',
-        {
-          development: process.env.BABEL_ENV !== 'build',
-        },
-      ],
-      '@babel/preset-typescript',
-    ],
-    env: {
-      build: {
-        ignore: [
-          '**/*.d.ts',
-          '**/*.test.js',
-          '**/*.test.jsx',
-          '**/*.test.ts',
-          '**/*.test.tsx',
-          '__snapshots__',
-          '__tests__',
-        ],
+module.exports = {
+  ...commonConfig,
+  output: {
+    ...commonConfig.output,
+    publicPath: 'http://localhost:8080/',
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      ...commonConfig.module.rules,
+      {
+        test: /\.(js|jsx|ts|tsx)?$/,
+        use: 'react-hot-loader/webpack',
+        include: /node_modules/,
       },
-    },
-    ignore: ['node_modules'],
-    plugins: [
-      '@babel/plugin-proposal-class-properties',
-      '@babel/plugin-proposal-object-rest-spread',
-      '@babel/plugin-transform-runtime',
-      'babel-plugin-styled-components',
     ],
-  }
+  },
+  devServer: {
+    index: 'index.html',
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers':
+        'X-Requested-With, content-type, Authorization',
+    },
+  },
+  plugins: [...commonConfig.plugins],
 }
