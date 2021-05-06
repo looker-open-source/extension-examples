c69966c390d1fbb4badf2b6fe3fc928741e3d919
Bryn Ryans <bryn.ryans@looker.com>
2021-05-06 13:11:11 -0700

    Add lint support to helloworld-ts project

--- react/typescript/helloworld-ts/config/license-header.js
+++ react/typescript/helloworld-ts/config/license-header.js
@@ -23,12 +23,3 @@
  SOFTWARE.
 
  */
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { App } from './App'
-
-window.addEventListener('DOMContentLoaded', (_) => {
-  const root = document.createElement('div')
-  document.body.appendChild(root)
-  ReactDOM.render(<App />, root)
-})
