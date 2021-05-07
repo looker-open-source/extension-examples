706939cd2d7597f68ab1e23defca40e5b75a77fc
Bryn Ryans <bryans99@users.noreply.github.com>
2021-05-07 12:17:21 -0700

    Test for hello world JS project (#36)
    Contains an ugly mock for ExtensionContext. Works for now but should be improved.

--- react/javascript/helloworld-js/.eslintrc.js
+++ react/javascript/helloworld-js/.eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    'jest/globals': true,
   },
   extends: [
     'eslint:recommended',
@@ -22,6 +23,7 @@ module.exports = {
   },
   plugins: [
     'react',
+    'jest',
     'jest-dom',
     'prettier',
     'react-hooks',
