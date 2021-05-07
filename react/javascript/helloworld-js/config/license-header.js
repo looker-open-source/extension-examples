bb77bea8d9357db7ea0c145ab675e5aace186185
Bryn Ryans <bryans99@users.noreply.github.com>
2021-05-06 21:52:53 -0700

    helloworld js linting (#31)
    Bring into line with other projects.
    
    Remove unneeded typescript types

--- react/javascript/helloworld-js/config/license-header.js
+++ react/javascript/helloworld-js/config/license-header.js
@@ -21,37 +21,3 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-
-import React, { useEffect, useState, useContext } from 'react'
-import { Space, ComponentsProvider, Text } from '@looker/components'
-import { ExtensionContext } from '@looker/extension-sdk-react'
-
-export const HelloWorld = () => {
-  const { core40SDK } = useContext(ExtensionContext)
-  const [message, setMessage] = useState()
-
-  useEffect(() => {
-    const initialize = async () => {
-      try {
-        const value = await core40SDK.ok(core40SDK.me())
-        setMessage(`Hello, ${value.display_name}`)
-      } catch (error) {
-        setMessage('Error occured getting information about me!')
-        console.error(error)
-      }
-    }
-    initialize()
-  }, [])
-
-  return (
-    <>
-      <ComponentsProvider>
-        <Space p="xxxxxlarge" width="100%" height="50vh" around>
-          <Text p="xxxxxlarge" fontSize="xxxxxlarge">
-            {message}
-          </Text>
-        </Space>
-      </ComponentsProvider>
-    </>
-  )
-}
