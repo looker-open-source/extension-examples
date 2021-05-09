cc2f7e0d7a9e0f29f7d1b2929b480b1eff4c8bb5
Bryn Ryans <bryn.ryans@looker.com>
2021-05-09 15:13:09 -0700

    Core SDK api demo extension example
    Demonstrates usage of core SDK api in an extension.

---
+++ react/javascript/demo-core-sdk/src/demos/Dashboards.jsx
@@ -0,0 +1,134 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2021 Looker Data Sciences, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import React, { useContext, useState } from 'react'
+import {
+  Button,
+  DataTable,
+  DataTableItem,
+  DataTableCell,
+  MessageBar,
+  Tooltip,
+  Spinner,
+} from '@looker/components'
+import { ExtensionContext2 } from '@looker/extension-sdk-react'
+import { DemoWrapper } from '../components'
+
+const title = 'Dashboards'
+const description = 'Demonstrates reading dashboards.'
+const code = `const { extensionSDK } = useContext(ExtensionContext2)
+const tempData = await coreSDK.ok(coreSDK.all_dashboards())`
+const codeSourceName = 'Dashboards.jsx'
+
+export const Dashboards = () => {
+  const [intent, setIntent] = useState()
+  const [message, setMessage] = useState()
+  const [loading, setLoading] = useState(false)
+  const [data, setData] = useState()
+  const { coreSDK, extensionSDK } = useContext(ExtensionContext2)
+
+  const updateMessage = (message, intent = 'inform') => {
+    setIntent(intent)
+    setMessage(message)
+  }
+
+  const loadData = async () => {
+    try {
+      setLoading(true)
+      setData(undefined)
+      setMessage(undefined)
+      const tempData = await coreSDK.ok(coreSDK.all_dashboards())
+      setData(tempData)
+      updateMessage('Dashboards loaded')
+    } catch (error) {
+      console.log(error)
+      updateMessage('Failed to load dashboards', 'critical')
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  const columns = [
+    {
+      id: 'id',
+      title: 'ID',
+      type: 'string',
+      size: 'small',
+    },
+    {
+      id: 'title',
+      title: 'Title',
+      type: 'string',
+      size: 'large',
+    },
+    {
+      id: 'slug',
+      title: 'Slug',
+      type: 'string',
+      size: 'large',
+    },
+  ]
+
+  const items = (data || [])
+    .filter(({ id }) => !isNaN(parseInt(id)))
+    .map((item) => {
+      const { id, title, slug } = item
+      return (
+        <DataTableItem
+          key={id}
+          id={id}
+          onClick={() =>
+            extensionSDK.openBrowserWindow(
+              `/dashboards-next/${id}`,
+              '__dashboards__'
+            )
+          }
+        >
+          <DataTableCell>{id}</DataTableCell>
+          <DataTableCell>{title}</DataTableCell>
+          <DataTableCell>{slug}</DataTableCell>
+        </DataTableItem>
+      )
+    })
+
+  return (
+    <DemoWrapper
+      title={title}
+      description={description}
+      code={code}
+      codeSourceName={codeSourceName}
+    >
+      <Tooltip content="Click to load and display dashboard data.">
+        <Button onClick={loadData} disabled={loading}>
+          Read dashboard data
+        </Button>
+      </Tooltip>
+      <DataTable columns={columns} caption="Dashboard items">
+        {items}
+      </DataTable>
+      {loading && <Spinner />}
+      {message && <MessageBar intent={intent}>{message}</MessageBar>}
+    </DemoWrapper>
+  )
+}
---
+++ react/javascript/demo-core-sdk/src/demos/LookImage.jsx
@@ -0,0 +1,103 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2021 Looker Data Sciences, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import React, { useContext, useState } from 'react'
+import { Button, MessageBar, Spinner, Tooltip } from '@looker/components'
+import { ExtensionContext2 } from '@looker/extension-sdk-react'
+import { DemoWrapper } from '../components'
+
+const title = 'Look Image'
+const description = 'Demonstrates rendering a Look image.'
+const code = `const { extensionSDK } = useContext(ExtensionContext2)
+const looks = await coreSDK.ok(coreSDK.all_looks('id'))
+const rand = Math.floor(Math.random() * looks.length)
+const value = await coreSDK.ok(
+  coreSDK.run_look({
+    look_id: looks[rand].id,
+    result_format: 'png',
+  })
+)`
+const codeSourceName = 'LookImage.jsx'
+
+export const LookImage = () => {
+  const [intent, setIntent] = useState()
+  const [message, setMessage] = useState()
+  const [loading, setLoading] = useState(false)
+  const [data, setData] = useState()
+  const { coreSDK } = useContext(ExtensionContext2)
+
+  const updateMessage = (message, intent = 'inform') => {
+    setIntent(intent)
+    setMessage(message)
+  }
+
+  const loadData = async () => {
+    try {
+      setMessage(undefined)
+      setData(undefined)
+      const looks = await coreSDK.ok(coreSDK.all_looks('id'))
+      if (looks.length > 0) {
+        setLoading(true)
+        const rand = Math.floor(Math.random() * looks.length)
+        const value = await coreSDK.ok(
+          coreSDK.run_look({
+            look_id: looks[rand].id,
+            result_format: 'png',
+          })
+        )
+        if (value instanceof Blob) {
+          setData(URL.createObjectURL(value))
+        } else {
+          setData(btoa(`data:image/png;base64,${value}`))
+        }
+        updateMessage('Got image')
+      } else {
+        updateMessage('No looks to render', 'critical')
+      }
+    } catch (error) {
+      console.log(error)
+      updateMessage('Failed to load look data', 'critical')
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return (
+    <DemoWrapper
+      title={title}
+      description={description}
+      code={code}
+      codeSourceName={codeSourceName}
+    >
+      <Tooltip content="Click to generate Look image">
+        <Button onClick={loadData} disabled={loading}>
+          Generate Look image
+        </Button>
+      </Tooltip>
+      {loading && <Spinner />}
+      {data && <img src={data} />}
+      {message && <MessageBar intent={intent}>{message}</MessageBar>}
+    </DemoWrapper>
+  )
+}
