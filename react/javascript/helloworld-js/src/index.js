bc928fa600487d4e1da2be71f66850000a6197c3
Meredith Dodge <meredithdodge@google.com>
2022-09-14 19:14:51 +0000

    fix: Looker Components Popover positioning

--- react/javascript/helloworld-js/src/index.js
+++ react/javascript/helloworld-js/src/index.js
@@ -30,6 +30,7 @@ import { App } from './App'
 
 window.addEventListener('DOMContentLoaded', (event) => {
   const root = document.createElement('div')
+  root.style.height = '100%'
   document.body.appendChild(root)
   ReactDOM.render(<App />, root)
 })
--- react/typescript/helloworld-ts/src/index.tsx
+++ react/typescript/helloworld-ts/src/index.tsx
@@ -29,6 +29,7 @@ import { App } from './App'
 
 window.addEventListener('DOMContentLoaded', (_) => {
   const root = document.createElement('div')
+  root.style.height = '100%'
   document.body.appendChild(root)
   ReactDOM.render(<App />, root)
 })
