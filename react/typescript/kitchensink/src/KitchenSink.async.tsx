0f968a98b73072212b074bc6b9a63b4c925fb3f4
Bryn Ryans <bryans99@users.noreply.github.com>
2023-10-02 10:15:52 -0700

    feat: improve code splitting example (#105)
    Updates code splitting example to reflect latest thinking. The
    ExtensionProvider40 is now loaded separately from the application
    reducing the initial bundle size from 1.1MB to 775kb. The thinking
    is that the ExtensionProvider should be loaded as quickly as possible
    so that it can communicate with the Looker host as soon as possible.

--- react/typescript/kitchensink/src/KitchenSink.async.tsx
+++ react/typescript/kitchensink/src/KitchenSink.async.tsx
@@ -24,29 +24,13 @@
 
  */
 
-import React, { useState, Suspense } from 'react'
-import { ExtensionProvider40 } from '@looker/extension-sdk-react'
-// Components loaded using code splitting
-import { AsyncKitchenSink as KitchenSink } from './KitchenSink.async'
+import React, { lazy } from 'react'
+import type { KitchenSinkProps } from './types'
 
-// If the Looker server does not support code splitting (version 7.20 and below) replace the above
-// imports with the imports below:
-// import { KitchenSink } from './KitchenSink'
+const KitchenSink = lazy<any>(
+  async () => import(/* webpackChunkName: "kitchensink" */ './KitchenSink')
+)
 
-export const App: React.FC = () => {
-  const [route, setRoute] = useState('')
-  const [routeState, setRouteState] = useState()
-
-  const onRouteChange = (route: string, routeState?: any) => {
-    setRoute(route)
-    setRouteState(routeState)
-  }
-
-  return (
-    <Suspense fallback={<></>}>
-      <ExtensionProvider40 onRouteChange={onRouteChange}>
-        <KitchenSink route={route} routeState={routeState} />
-      </ExtensionProvider40>
-    </Suspense>
-  )
-}
+export const AsyncKitchenSink: React.FC<KitchenSinkProps> = (props) => (
+  <KitchenSink {...props} />
+)
